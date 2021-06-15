93251427384ccef893f929b66201727e391f72e0
Shivansh Goel <shivanshgoel21@gmail.com>
2021-06-16 00:33:38 +0530

    call.view.js: add ```calloffer``` route

--- views/call/call.view.js
+++ views/call/call.view.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const call = express.Router()
+const createError = require('http-errors')
+const { db } = require("../../setup/firebase/firebase.setup")
+const { v4: uuid4 } = require('uuid')
 //const { verifyAccessToken } = require('../../controller/auth/auth.controller')
 
 
@@ -9,20 +12,50 @@ const call = express.Router()
 let socket
 const callService = require('../../services/call/call.service')
 
-call.get('/', (req, res, next) => {
+call.post('/calloffer', (req, res, next) => {
 
+    try{
+        let { sender, receiver } = req.body
+
+        if(!sender || !receiver) throw createError.BadRequest('Bad Request')
+
+        console.log('Call Service in Action')
+
+        // global event emitter
+        const eventEmitter = req.app.get('eventEmitter')
+
+        // emit an event to make a call offer
+        eventEmitter.emit('send-call-offer', {sender: sender, receiver: receiver, roomId: uuidV4() })
+
+        res.send(true)
+
+    } catch(error){
+        next(error)
+    }
     
-    let sender = 'shivansh'
-    let receiver = 'karan'
-    console.log('Call Service in Action')
+})
 
-    // global event emitter
-    const eventEmitter = req.app.get('eventEmitter')
+call.get('/contactlist', (req, res, next)=>{
 
-    // make a call offer
-    eventEmitter.emit('send-call-offer', {sender: sender, receiver: receiver})
-    
-    res.send('new call')
+    try{
+        let constactList
+        db.collection('users').get().then((data)=>{
+            constactList = data.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    email: doc.data().email
+                }   
+            })
+
+            res.json(constactList)
+        }).catch((err)=>{
+            next(err)
+        })
+        
+    } catch(error){
+        next(error)
+    }
+
 })
 
 
