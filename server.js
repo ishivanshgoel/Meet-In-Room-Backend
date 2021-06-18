302dbd917f6a0477cc12ba0c94b10bc927220d5f
Shivansh Goel <shivanshgoel21@gmail.com>
2021-06-18 23:02:15 +0530

    server.js: minor changes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const bodyparser = require('body-parser')
+const createErrors = require('http-errors')
+const { ExpressPeerServer } = require('peer');
 const Emitter = require('events')
 
 // Event emitter
@@ -32,6 +34,7 @@ app.use(async (req, res, next)=>{
 
 // error handler
 app.use((err, req, res, next)=>{
+    console.log(err)
     res.status(err.status || 500)
     res.send({
         error: {
@@ -41,12 +44,19 @@ app.use((err, req, res, next)=>{
     })
 })
 
-
+const POST = process.env.PORT || 5000
 
 const server = app.listen((5000), ()=>{
     console.log('Server Running at port 5000')
 })
 
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+    allow_discovery: true
+  });
+
+  
+app.use('/peerjs', peerServer);
 
 //////////// socket server ////////////////////////
 
@@ -59,8 +69,11 @@ const io = require('socket.io')(server, {
     }
 })
 
+let socketGlobal
 
 io.on('connection', (socket)=>{
+
+    socketGlobal=socket
 
     // join the user
     socket.on('join', (userid)=>{
@@ -73,10 +86,7 @@ io.on('connection', (socket)=>{
     socket.on('accepted-call-offer', (data)=>{
         console.log('Call Accepted', data)
         io.to(data.sender).emit('call-offer-accepted', data)
-
-        // join the room
-
-    
+        // io.to(data.roomId).broadcast.emit('user-connected', data.receiver)
     })
 
     // emit rejected offer to sender
@@ -85,14 +95,28 @@ io.on('connection', (socket)=>{
         io.to(data.sender).emit('call-offer-rejected', data)
     })
 
+    // joining room
+    socket.on('join-room', (roomId, userId) => {
+        console.log("Room Joined ", roomId)
+        console.log("Room joined by user ", userId)
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+    
+        socket.on('disconnect', () => {
+          socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        })
+    })
 
 })
-
 
 /////////////// event listners ////////////////////
 eventEmitter.on('send-call-offer', (data)=>{
-
+    // socketGlobal.join(data.roomId)
     // emitting event to reciever
+    // console.log(data)
     io.to(data.receiver).emit('receive-call-offer', data)
 
 })
+
+
+
